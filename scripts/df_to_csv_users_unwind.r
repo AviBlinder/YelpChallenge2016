394a176d84dc3fb28ca8b7eb3f3490e525f81155
Avi Blinder <aviblinder@gmail.com>
2016-10-16 16:12:52 +0300

    fixes to unwind

--- scripts/df_to_csv_users_unwind.r
+++ scripts/df_to_csv_users_unwind.r
@@ -1,12 +1,18 @@
 rm(list=ls())
 users <- readRDS("./data/users_df_part2")
 
-unwind <- function(user,idx){
-  unwinded_friends <- unlist(user$friends[idx])
-  friends_list <- data.frame()
-  friends_list <- cbind(user$user_id[idx],unwinded_friends)
-  return (friends_list)
+unwind <- function(df,col1,col2,idx){
+  unwinded_column <- unlist(df[idx,col2])
+  unwinded_list <- data.frame()
+  unwinded_list <- data.frame(cbind(df[idx,col1],unwinded_column))
+  if ( (dim(unwinded_list)[2]) == 2){  
+    names(unwinded_list) <- c(col1,col2)
+  } else{
+    unwinded_list <- data.frame()
+  }
+  return (unwinded_list)
 }
+
 
 
 ###
@@ -14,9 +20,7 @@ strt<-Sys.time()
 friends1 <- data.frame()
 for (i in 1:100000) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends1 <- rbind(friends1,unwind(users,i))  
-  }
+    friends1 <- rbind(friends1,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends1,"./data/friends1.csv",row.names = FALSE)
@@ -26,9 +30,7 @@ strt<-Sys.time()
 friends2 <- data.frame()
 for (i in 100001:200000) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends2 <- rbind(friends2,unwind(users,i))  
-  }
+    friends2 <- rbind(friends2,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends2,"./data/friends2.csv",row.names = FALSE)
@@ -37,9 +39,7 @@ strt<-Sys.time()
 friends3 <- data.frame()
 for (i in 200001:300000) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends3 <- rbind(friends3,unwind(users,i))  
-  }
+    friends3 <- rbind(friends3,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends3,"./data/friends3.csv",row.names = FALSE)
@@ -48,9 +48,7 @@ strt<-Sys.time()
 friends4 <- data.frame()
 for (i in 300001:400000) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends4 <- rbind(friends4,unwind(users,i))  
-  }
+    friends4 <- rbind(friends4,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends4,"./data/friends4.csv",row.names = FALSE)
@@ -59,9 +57,7 @@ strt<-Sys.time()
 friends5 <- data.frame()
 for (i in 400001:500000) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends5 <- rbind(friends5,unwind(users,i))  
-  }
+    friends5 <- rbind(friends5,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends5,"./data/friends5.csv",row.names = FALSE)
@@ -70,9 +66,7 @@ strt<-Sys.time()
 friends6 <- data.frame()
 for (i in 500001:600000) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends6 <- rbind(friends6,unwind(users,i))  
-  }
+    friends6 <- rbind(friends6,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends6,"./data/friends6.csv",row.names = FALSE)
@@ -81,9 +75,7 @@ strt<-Sys.time()
 friends7 <- data.frame()
 for (i in 600001:nrow(users)) {
   cat(i,"")
-  if ( length(users$friends[[i]]) > 0){
-    friends7 <- rbind(friends7,unwind(users,i))  
-  }
+    friends7 <- rbind(friends7,unwind(users,"user_id","friends",i))  
 }
 print(Sys.time()-strt)
 write.csv(friends7,"./data/friends7.csv",row.names = FALSE)
